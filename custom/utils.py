1b989258c910548a891b4d987376eac2759a17c4
Aitor Sánchez <aitorsq24@gmail.com>
2023-04-21 18:25:00 +0200

    new files

--- custom/utils.py
+++ custom/utils.py
@@ -187,7 +187,7 @@ def create_dataset(list_data,num_copies,adj_size):
     return list_graph, list_n_sequence, list_node_num, cent_mat, deg_mat
 
 
-def graph_to_adj_bet(list_graph,list_n_sequence,list_node_num,model_size):
+def graph_to_adj_bet(list_graph,list_n_sequence,list_node_num,model_size,showing_zeros = False):
     
     list_adjacency = list()
     list_adjacency_t = list()
@@ -196,9 +196,10 @@ def graph_to_adj_bet(list_graph,list_n_sequence,list_node_num,model_size):
     zero_list = list()
     list_rand_pos = list()
     list_sparse_diag = list()
-    
+    print(f"Processing {len(list_graph)} graphs...")
     for i in range(len(list_graph)):
-        print(f"Processing graphs: {i+1}/{len(list_graph)}",end='\r')
+        #print(f"Processing graphs: {i+1}/{len(list_graph)}")
+        
         graph = list_graph[i]
         edges = list(graph.edges())
         graph = nx.MultiDiGraph()
@@ -227,6 +228,7 @@ def graph_to_adj_bet(list_graph,list_n_sequence,list_node_num,model_size):
         
         non_zero_ind = np.nonzero(degree_arr.flatten())
         non_zero_ind = non_zero_ind[0]
+        total_zero_beforehand = node_num-len(non_zero_ind)
         
         g_nkit = nx2nkit(graph)
         
@@ -241,9 +243,12 @@ def graph_to_adj_bet(list_graph,list_n_sequence,list_node_num,model_size):
            
             is_zero = clique_check(index,node_sequence,all_out_dict,all_in_dict)
             if is_zero == True:
-              
+                total_zero_beforehand += 1
                 degree_arr[index,0]=0.0
-                    
+
+        if showing_zeros:
+            print(f"Graph {i}, Nodes: {node_num}, Beforehand zeros: {total_zero_beforehand} ({(total_zero_beforehand/node_num)*100} %)")
+
         adj_temp = adj_temp.multiply(csr_matrix(degree_arr))
         adj_temp_t = adj_temp_t.multiply(csr_matrix(degree_arr))
                 
@@ -270,7 +275,7 @@ def graph_to_adj_bet(list_graph,list_n_sequence,list_node_num,model_size):
         
         adj_mat_t = sparse_mx_to_torch_sparse_tensor(adj_mat_t)
         list_adjacency_t.append(adj_mat_t)
-    print("")          
+       
     return list_adjacency,list_adjacency_t
 
 
@@ -338,8 +343,6 @@ def train(list_adj_train,list_adj_t_train,list_num_node_train,bc_mat_train,model
         optimizer.step()
 
 
-
-
 def test(list_adj_test,list_adj_t_test,list_num_node_test,bc_mat_test,deg_mat_test,model,device,size):
     model.eval()
     loss_val = 0
@@ -368,6 +371,55 @@ def test(list_adj_test,list_adj_t_test,list_num_node_test,bc_mat_test,deg_mat_te
 
     print(f"   Average KT score on test graphs is: {np.mean(np.array(list_kt))} and std: {np.std(np.array(list_kt))}")
     return {"kt":np.mean(np.array(list_kt)), "std":np.std(np.array(list_kt))}
+
+
+def nozeros(a,b):
+    aout = []
+    bout = []
+    
+    for idx,v in enumerate(b):
+        if v != 0:
+            aout.append(a[idx])
+            bout.append(b[idx])
+    
+    return aout,bout
+
+
+def test_nozeros(list_adj_test,list_adj_t_test,list_num_node_test,bc_mat_test,deg_mat_test,model,device,size):
+    model.eval()
+    loss_val = 0
+    list_kt = list()
+    num_samples_test = len(list_adj_test)
+    for j in range(num_samples_test):
+        adj = list_adj_test[j]
+        adj_t = list_adj_t_test[j]
+        adj=adj.to(device)
+        adj_t = adj_t.to(device)
+        num_nodes = list_num_node_test[j]
+        
+        y_out = model(adj,adj_t)
+    
+        
+        true_arr = torch.from_numpy(bc_mat_test[:,j]).float()
+        true_val = true_arr.to(device)
+
+        deg_arr = torch.from_numpy(deg_mat_test[:,j]).float()
+        deg_val = deg_arr.to(device)  
+
+        y_out = y_out.reshape((size))
+        true_val = true_val.reshape((size))
+        predict_arr = y_out.cpu().detach().numpy()
+        true_arr = true_val.cpu().detach().numpy()
+        
+        a,b = nozeros(predict_arr[:num_nodes],true_arr[:num_nodes])
+        kt,_ = kendalltau(a,b)
+        list_kt.append(kt)
+        #g_tmp = list_graph_test[j]
+        #print(f"Graph stats:{g_tmp.number_of_nodes()}/{g_tmp.number_of_edges()},  KT:{kt}")
+
+    print(f"   Average KT score on test graphs is: {np.mean(np.array(list_kt))} and std: {np.std(np.array(list_kt))}")
+    return {"kt":np.mean(np.array(list_kt)), "std":np.std(np.array(list_kt))}
+
 
 
 def test_onegraph(list_adj_test,list_adj_t_test,list_num_node_test,bc_mat_test,deg_mat_test,model,device,size):
@@ -401,3 +453,40 @@ def test_onegraph(list_adj_test,list_adj_t_test,list_num_node_test,bc_mat_test,d
         deg = deg_val.cpu().detach().numpy()
 
         return {'pred': pred[:num_nodes], 'true': real[:num_nodes],'deg': deg[:num_nodes],'kt': kendalltau(pred[:num_nodes],real[:num_nodes])[0]}
+
+
+def test_onegraph_nozeros(list_adj_test,list_adj_t_test,list_num_node_test,bc_mat_test,deg_mat_test,model,device,size):
+    model.eval()
+    loss_val = 0
+    list_kt = list()
+    num_samples_test = len(list_adj_test)
+
+    for j in range(num_samples_test):
+        adj = list_adj_test[j]
+        adj_t = list_adj_t_test[j]
+        adj=adj.to(device)
+        adj_t = adj_t.to(device)
+        num_nodes = list_num_node_test[j]
+        
+        y_out = model(adj,adj_t)
+    
+        
+        true_arr = torch.from_numpy(bc_mat_test[:,j]).float()
+        true_val = true_arr.to(device)
+
+        deg_arr = torch.from_numpy(deg_mat_test[:,j]).float()
+        deg_val = deg_arr.to(device) 
+
+        y_out = y_out.reshape((size))
+        true_val = true_val.reshape((size))
+        deg_val = deg_val.reshape((size))
+
+        pred = y_out.cpu().detach().numpy()
+        real = true_val.cpu().detach().numpy()
+        deg = deg_val.cpu().detach().numpy()
+
+        a,b = nozeros(pred[:num_nodes],real[:num_nodes])
+        kt,_ = kendalltau(a,b)
+        list_kt.append(kt)
+
+        return {'pred': pred[:num_nodes], 'true': real[:num_nodes],'deg': deg[:num_nodes],'kt': kt}
